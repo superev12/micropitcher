67f0de8e0f9807250ad19bdc68eb130a8bc2a0e9
superev12 <evan.snoswell@gmail.com>
2022-04-07 15:24:15 +0100

    remove demo projects

--- src/Parameters.h
+++ src/Parameters.h
@@ -8,22 +8,12 @@ struct Parameters
     {
         processor.addParameter(gain);
         processor.addParameter(enable);
-        processor.addParameter(thing);
-        processor.addParameter(pan);
-
-
     }
 
     //Raw pointers. They will be owned by either the processor or the APVTS (if you use it)
     juce::AudioParameterFloat* gain =
-        new juce::AudioParameterFloat("Gain", "Gain", 0.f, 1.f, 0.5f);
-
-    juce::AudioParameterFloat* pan =
-        new juce::AudioParameterFloat("Pan", "Pan", 0.f, 1.f, 0.5f);
+        new juce::AudioParameterFloat("Gain", "Not your pappa's gain", 0.f, 1.f, 0.5f);
 
     juce::AudioParameterBool* enable =
         new juce::AudioParameterBool("Enable", "Enable", true);
-
-    juce::AudioParameterBool* thing =
-        new juce::AudioParameterBool("Thing", "Thing", true);
 };
